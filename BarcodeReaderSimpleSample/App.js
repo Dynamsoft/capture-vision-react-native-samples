252aeff0c3282a59f1d9b0e507611b48ab34dfb7
Jerson <32625406+Dynamsoft-Jerson@users.noreply.github.com>
2022-06-07 11:22:19 +0800

    fix bug

--- BarcodeReaderSimpleSample/App.js
+++ BarcodeReaderSimpleSample/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
 
     async componentWillUnmount() {
         await this.reader.stopScanning()
-        this.reader.removeResultListener()
+        this.reader.removeAllResultListeners()
     }
 
 
