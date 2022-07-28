2c62977d02fad651111c837112abba11aa5afd2a
arckongem <arckongem@gmail.com>
2022-07-28 17:08:10 +0800

    remove scanRegion

--- BarcodeReaderSimpleSample/BarcodeScanner.js
+++ BarcodeReaderSimpleSample/BarcodeScanner.js
@@ -50,14 +50,6 @@ class BarcodeScanner extends React.Component {
 
   render() {
     let barcode_text = '';
-    // Define the scan region.
-    let region = {
-      regionTop: 30,
-      regionLeft: 15,
-      regionBottom: 70,
-      regionRight: 85,
-      regionMeasuredByPercentage: true,
-    };
     let results = this.state.results;
     if (results && results.length > 0) {
       for (var i = 0; i < results.length; i++) {
@@ -77,8 +69,6 @@ class BarcodeScanner extends React.Component {
           this.scanner = ref;
         }}
         overlayVisible={true}
-        scanRegionVisible={true}
-        scanRegion={region}
         torchButton={{
           visible: true
         }}
