866c76c8a22eaf06385caf25a413ccee7afb1ca8
Dynamsoft-Henry <henry@dynamsoft.com>
2022-12-19 17:24:36 +0800

    Added API descriptions

--- js/BasicStructures.tsx
+++ js/BasicStructures.tsx
@@ -1,23 +1,60 @@
 export interface Quadrilateral {
+    /**
+    * Vertex coordinates of the quadrilateral.
+    */
     points : Point[];
 }
 
 export interface Point {
+    /**
+    * X coordinate of the point.
+    */
     x:number;
+    /**
+    * Y coordinate of the point.
+    */
     y:number;
 }
 
 export interface Region {
+    /**
+    * Y coordinate of the buttom border of the region.
+    */
     regionBottom: number;
+    /**
+    * X coordinate of the right border of the region.
+    */
     regionRight: number;
+    /**
+    * X coordinate of the left border of the region.
+    */
     regionLeft: number;
+    /**
+    * Y coordinate of the top border of the region.
+    */
     regionTop: number;
+    /**
+    * 1/True: the coordinates are measured by percentage.
+    * 0/False: the coordinates are measured by pixel distance.
+    */
     regionMeasuredByPercentage: number | boolean;
 }
 
 export interface Rect {
+    /**
+    * X coordinate of the top-left vertex of the rectangle.
+    */
     x: number;
+    /**
+    * Y coordinate of the top-left vertex of the rectangle.
+    */
     y: number;
+    /**
+    * The width of the rectangle.
+    */
     width: number;
+    /**
+    * The height of the rectangle.
+    */
     height: number;
 }
--- js/CameraSettings.tsx
+++ js/CameraSettings.tsx
@@ -1,19 +1,46 @@
 import { Rect } from "./BasicStructures";
 
 export interface TorchButton {
+    /**
+    * Defines the location of the torch button.
+    */
     location: Rect;
+    /**
+    * True: the torch will be displayed.
+    * False: the torch will be hide.
+    */
     visible: boolean;
+    /**
+    * A base 64 string that specify the torch on image.
+    * When the torch is on, this image will be displayed as the torch image.
+    */
     torchOnImageBase64: string;
+    /**
+    * A base 64 string that specify the torch off image.
+    * When the torch is off, this image will be displayed as the torch image.
+    */
     torchOffImageBase64: string;
 }
 
 
 export enum EnumTorchState {
+    /**
+    * Set the torch state to off.
+    */
     OFF,
+    /**
+    * Set the torch state to on.
+    */
     ON
 }
 
 export enum EnumCameraPosition {
+    /**
+    * Use the back-facing camera.
+    */
     CP_BACK,
+    /**
+    * Use the front-facing camera.
+    */
     CP_FRONT
 }
